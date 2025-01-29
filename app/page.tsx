d27841d164d633db63dd52353668000396a7769e
John Paul Imperial <121214456+PaulJohn25@users.noreply.github.com>
2025-01-29 09:18:53 +0800

    Add baseUrl on tsconfig.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Header from "@/app/ui/header/Header";
-import HeroSection from "@/app/ui/hero/Hero";
-import AboutSection from "@/app/ui/about/About";
-import FeaturesSection from "@/app/ui/features/Features";
-import FaqSection from "@/app/ui/faq/Faq";
-import CTASection from "@/app/ui/cta/CTA";
+import HeroSection from "@/app/ui/hero/HeroSection";
+import AboutSection from "@/app/ui/about/AboutSection";
+import FeaturesSection from "@/app/ui/features/FeaturesSection";
+import FaqSection from "@/app/ui/faq/FaqSection";
+import CTASection from "@/app/ui/cta/CTASection";
 import Footer from "@/app/ui/footer/Footer";
 
 export default function Home() {
